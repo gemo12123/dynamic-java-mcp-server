4b4c60a3f052cc82fcc26b0c47079ca97a40f38a
gemo <1151641026@qq.com>
2025-05-29 22:39:24 +0800

    first commit

---
+++ webflux-dynamic-mcp-server/src/main/java/org/mytest/test/WebfluxApplication.java
@@ -0,0 +1,7 @@
+package org.mytest.test;
+
+public class WebfluxApplication {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
---
+++ webmvc-dynamic-mcp-server/src/main/java/org/mytest/test/WebmvcApplication.java
@@ -0,0 +1,14 @@
+package org.mytest.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author gemo
+ */
+@SpringBootApplication
+public class WebmvcApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WebmvcApplication.class, args);
+    }
+}
