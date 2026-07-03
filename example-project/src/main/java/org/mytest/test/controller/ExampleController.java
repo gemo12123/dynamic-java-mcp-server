8a5eb6b500b3f484283ac8b7fe62e775ece1d26c
gemo <1151641026@qq.com>
2026-07-03 11:06:33 +0800

    补充样例

--- example-project/src/main/java/org/mytest/test/controller/ExampleController.java
+++ example-project/src/main/java/org/mytest/test/controller/ExampleController.java
@@ -2,7 +2,11 @@ package org.mytest.test.controller;
 
 import org.mytest.test.annotation.Tool;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 /**
  * @author gemo
@@ -15,5 +19,11 @@ public class ExampleController {
     @Tool(name = "helloWorld")
     public String helloWorld(){
         return "hello world!";
+    }
+
+    @PostMapping("/hello-world-post")
+    @Tool(name = "hello-world-post")
+    public String post(@RequestBody Map<String, Object> map){
+        return String.valueOf(map);
     }
 }
